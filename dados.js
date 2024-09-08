41515f47182900e7407549ddd0f7ac83effecc1b
Cyntia Saray <108286929+cyntiasarayengineer@users.noreply.github.com>
2024-09-08 20:29:33 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,47 @@
+let dados = [
+    {
+        titulo: "Sidney Sheldon",
+        descricao: "Um dos autores mais vendidos de todos os tempos, conhecido por seus romances de suspense e mistério.",
+        link: "https://pt.wikipedia.org/wiki/Sidney_Sheldon",
+        tags: "sheldon, suspense, mistério, o_jogo_de_mestre, o_terceiro_dia",
+        livros: [
+            {
+                livro: "Conte-me Seus Sonhos",
+                resenha: [
+                    "Sidney Sheldon nos presenteia com mais um thriller psicológico em Conte-me Seus Sonhos. A trama gira em torno de três belas jovens acusadas de uma série de assassinatos brutais. O julgamento que se segue é nada convencional, com provas médicas intrigantes que desafiam a compreensão da realidade."
+                ]
+            },
+            // Outros livros de Sidney Sheldon
+        ]
+    },
+    {
+        titulo: "Machado de Assis",
+        descricao: "Considerado um dos maiores escritores brasileiros, Machado de Assis é conhecido por suas obras realistas e críticas sociais.",
+        link: "https://pt.wikipedia.org/wiki/Machado_de_Assis",
+        tags: "machado_de_assis, realismo, literatura_brasileira, dom_casmurro, memórias_póstumas_de_brás_cubas",
+        livros: [
+            {
+                livro: "Dom Casmurro",
+                resenha: [
+                    "Dom Casmurro, a obra-prima de Machado de Assis, é uma jornada introspectiva e tortuosa através da mente de Bento Santiago. Narrada em primeira pessoa, a história nos apresenta um homem atormentado pelo ciúme e pela dúvida, que busca incessantemente desvendar a verdade sobre a suposta traição de sua esposa, Capitu."
+                ]
+            },
+            // Outros livros de Machado de Assis
+        ]
+    },
+    {
+        titulo: "G.K. Chesterton",
+        descricao: "Escritor inglês, conhecido por seus romances policiais, ensaios e obras de fantasia.",
+        link: "https://pt.wikipedia.org/wiki/G._K._Chesterton",
+        tags: "chesterton, policial, fantasia, padre_brown, o_homem_que_foi_quinta",
+        livros: [
+            {
+                livro: "O Homem Eterno",
+                resenha: [
+                    "G.K. Chesterton, com sua prosa vibrante e mente aguda, nos presenteia em O Homem Eterno com uma defesa apaixonada e original do cristianismo. Longe de ser um tratado teológico convencional, o livro é uma jornada intelectual que busca desvendar a natureza única do ser humano e seu lugar no universo."
+                ]
+            },
+            // Outros livros de G.K. Chesterton
+        ]
+    }
+];
